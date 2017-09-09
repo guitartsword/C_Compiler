14f6b444b894826860204bb0f5781a8befac712d
Isaias Valle <guitartsword@gmail.com>
2017-09-09 14:16:25 -0600

    main.c file

---
+++ main.c
@@ -0,0 +1,28 @@
+#include "stdio.h"
+/*This is a multi-line comment
+ which ends here
+ */
+int count = 0;
+void printAndCount(char * string);
+
+int main(int argc, char*[] args){
+    int aritmetica = 3+3+6/2*3
+    if(3=='3'){
+        int arit = 3;
+    }else{
+        char x = 'i';
+    }
+    //printf scanf
+    printf("hola %d",3);
+    scanf(arit,3);
+    char x='2';
+    int x=2;
+    bool pene = false;
+    //"yo no soy un " String literal"
+    char* string = "yo \"soy\" un String";
+    //Returns 0, no error, oneline comment
+    return 0;
+}
+void printAndCount(char* string){
+    printf("%d: %s", count, string);
+}
