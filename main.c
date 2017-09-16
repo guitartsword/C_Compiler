db99b2f23b84d33c2ee29e80b1c0757f4e90132a
melvinchun <melvin.manuel.portillo@gmail.com>
2017-09-16 16:10:24 -0600

    Probando
    Todo:
    1. Arreglar if dentro del main.
    2. Poder manejar llamado de funciones dentro del main. 
    3. agregar token args*[]

--- main.c
+++ main.c
@@ -1,26 +1,21 @@
 char xsad;
 int count = 0;
-if(3>3){
-    int x;
-}
+
 void printAndCount(char * string);
 
-int main(int argc, char*[] args){
-    int aritmetica = 3+3+6/2*3
+int main(int argc, char* args){
+    int aritmetica = 3+3+6/2*3;
+    char x= '2';
+    int x= 2;
+    bool pene = false;
+    //"yo no soy un " String literal"
+    char* string = "yo \"soy\" un String";
+    //Returns 0, no error, oneline comment
     if(3=='3'){
         int arit = 3;
     }else{
         char x = 'i';
     }
-    //printf scanf
-    printf("hola %d",3);
-    scanf(arit,3);
-    char x='2';
-    int x=2;
-    bool pene = false;
-    //"yo no soy un " String literal"
-    char* string = "yo \"soy\" un String";
-    //Returns 0, no error, oneline comment
     return 0;
 }
 void printAndCount(char* string){
