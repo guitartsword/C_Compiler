578246564c7c7bd0fc87a931e249d942da16747d
Isaias Valle <guitartsword@gmail.com>
2017-12-17 12:05:10 -0600

    added Intermediate Code

--- test/prueba.c
+++ test/prueba.c
@@ -8,11 +8,12 @@ int main(){
     printf("a=%d\n", a);
     a--;
   }
-  algo(&a);
+  algo(&a,b,b);
+  valor(a,b,a);
   for(b=0; b<3; b++){
     {int x;
     x=3;}
-    {int x;x=4;}
+    {int x;x=4;}{int x = 3;}
     printf("for_a=%d\n",a);
   }
   if (a == 3 && b == 2 ) {
@@ -22,15 +23,24 @@ int main(){
   }
   printf("a=%d\n", a);
   printf("b=%d\n", b);
+  debug();
   return 0;
 }
-
+void debug(){
+  printf("debugging\n");
+}
 int algo(int* x, int y, int z){
   *x=3;
+  y = z;
+  printf("x=%d",x);
+  printf("*x=%d",*x);
   return 0;
 }
-int valor(int x){
+int valor(int x, int y, int z){
   int h,a,r;
-  x=3;
+  h=2;
+  a=3;
+  x=h*a;
+  r= h+x*(h+a);
   return 0;
 }
